7dfa2c805718171ee7e19b94a0407527bf1d9d11
Weiqing He <weiqing726@outlook.com>
2024-08-18 16:45:24 -0400

    3 --test

--- sequence_detectgpt.py
+++ sequence_detectgpt.py
@@ -175,6 +175,4 @@ with open(f"score/score_{save_name}_{args.prob_threshold}_{args.sim_threshold}_{
 with open(f"pool/pool_{save_name}_{args.prob_threshold}_{args.sim_threshold}_{args.pool_size}_detectgpt.pkl", 'wb') as f:
     pickle.dump(pool_num, f)
 
-print('finish')
-
 
