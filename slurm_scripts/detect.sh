57e984182a300137bd14e7afd301394eea6f665e
Weiqing He <weiqing726@outlook.com>
2024-09-02 15:33:40 -0400

    slurm update pool size change to float

--- slurm_scripts/detect.sh
+++ slurm_scripts/detect.sh
@@ -5,7 +5,7 @@
 #SBATCH      --mem=80G
 #SBATCH      --gpus=a40
 #SBATCH      --mail-type=ALL
-#SBATCH      --array=1-4
+#SBATCH      --array=1-12
 #SBATCH      --output=slurm_output/output_%A_%a.log
 
 
@@ -20,22 +20,62 @@ case $SLURM_ARRAY_TASK_ID in
   1)
     DATA="data/gpt-4o-mini.jsonl_pp"
     CA="detect_cache/detectgpt_gpt_4o-mini_cache.json"
-    P=1
+    P=3
     ;;
   2)
     DATA="data/gpt2_xl.jsonl_pp"
     CA="detect_cache/detectgpt_gpt2_xl_cache.json"
-    P=1
+    P=3
     ;;
   3)
     DATA="data/gpt3.jsonl_pp"
     CA="detect_cache/detectgpt_gpt3_cache.json"
-    P=1
+    P=3
     ;;
   4)
     DATA="data/opt_13b.jsonl_pp"
     CA="detect_cache/detectgpt_opt_13b_cache.json"
-    P=1
+    P=3
+    ;;
+  5)
+    DATA="data/gpt-4o-mini.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt_4o-mini_cache.json"
+    P=2
+    ;;
+  6)
+    DATA="data/gpt2_xl.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt2_xl_cache.json"
+    P=2
+    ;;
+  7)
+    DATA="data/gpt3.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt3_cache.json"
+    P=2
+    ;;
+  8)
+    DATA="data/opt_13b.jsonl_pp"
+    CA="detect_cache/detectgpt_opt_13b_cache.json"
+    P=2
+    ;;
+  9)
+    DATA="data/gpt-4o-mini.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt_4o-mini_cache.json"
+    P=1.5
+    ;;
+  10)
+    DATA="data/gpt2_xl.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt2_xl_cache.json"
+    P=1.5
+    ;;
+  11)
+    DATA="data/gpt3.jsonl_pp"
+    CA="detect_cache/detectgpt_gpt3_cache.json"
+    P=1.5
+    ;;
+  12)
+    DATA="data/opt_13b.jsonl_pp"
+    CA="detect_cache/detectgpt_opt_13b_cache.json"
+    P=1.5
     ;;
 esac
 
