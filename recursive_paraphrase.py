429d63516d0fad8ce18b07a748659fd1df846543
Weiqing He <weiqing726@outlook.com>
2024-09-03 19:08:54 -0400

    recursive paraphrase

---
+++ recursive_paraphrase.py
@@ -0,0 +1,107 @@
+import argparse
+import json
+import nltk
+import time
+import os
+import tqdm
+
+from nltk.tokenize import sent_tokenize
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+import torch
+
+from utils import form_partitions
+
+nltk.download('punkt')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--output_file', default="data/gpt2_xl.jsonl_pp")
+parser.add_argument('--para_src', default="paraphrase_outputs")
+parser.add_argument('--para_dst', default="paraphrase_outputs_2")
+parser.add_argument('--model', type=str, default='kalpeshk2011/dipper-paraphraser-xxl', help='data to paraphrase')
+parser.add_argument("--refresh", action='store_true', help="Renew the data")
+parser.add_argument('--num_shards', type=int, default=1, help='data to paraphrase')
+parser.add_argument('--local_rank', type=int, default=0, help='data to paraphrase')
+parser.add_argument('--sent_interval', type=int, default=3, help='data to paraphrase')
+args = parser.parse_args()
+
+# read args.output_file
+with open(args.output_file, "r") as f:
+    data = [json.loads(x) for x in f.read().strip().split("\n")]
+
+if "no-ctx" in args.model:
+    args.output_file = args.output_file + "_no_ctx"
+
+if args.sent_interval == 1:
+    args.output_file = args.output_file + "_sent"
+
+if args.num_shards > 1:
+    partitions = form_partitions(data, args.num_shards)
+    data = partitions[args.local_rank]
+    output_file = f'{args.output_file}_pp.shard_{args.local_rank}'
+else:
+    output_file = args.output_file + f"_pp{args.para_dst[-1]}"
+
+if os.path.exists(output_file):
+    with open(output_file, "r") as f:
+        num_output_points = len([json.loads(x) for x in f.read().strip().split("\n")])
+else:
+    num_output_points = 0
+print(f"Skipping {num_output_points} points")
+
+time1 = time.time()
+tokenizer = T5Tokenizer.from_pretrained('google/t5-v1_1-xxl')
+model = T5ForConditionalGeneration.from_pretrained(args.model)
+print("Model loaded in ", time.time() - time1)
+model.cuda()
+model.eval()
+
+# iterate over data and tokenize each instance
+for idx, dd in tqdm.tqdm(enumerate(data), total=len(data)):
+    if idx < num_output_points:
+        continue
+    # tokenize prefix
+    if args.refresh or args.para_dst not in dd:
+        paraphrase_outputs_k = {}
+        for lex, order in [(40, 40)]:
+            if isinstance(dd[args.para_src]['lex_40_order_40']['output'], str):
+                input_gen = dd[args.para_src]['lex_40_order_40']['output'].strip()
+            else:
+                input_gen = dd[args.para_src]['lex_40_order_40']['output'][0].strip()
+
+            # remove spurious newlines
+            input_gen = " ".join(input_gen.split())
+            sentences = sent_tokenize(input_gen)
+            prefix = " ".join(dd['prefix'].replace("\n", " ").split())
+            output_text = ""
+            final_input_text = ""
+
+            for sent_idx in range(0, len(sentences), args.sent_interval):
+                curr_sent_window = " ".join(sentences[sent_idx:sent_idx + args.sent_interval])
+                if "no-context" in args.model:
+                    final_input_text = f"lexical = {lex}, order = {order} {curr_sent_window}"
+                else:
+                    final_input_text = f"lexical = {lex}, order = {order} {prefix} <sent> {curr_sent_window} </sent>"
+
+                if idx == 0 and lex == 40 and order == 40:
+                    print(final_input_text)
+
+                final_input = tokenizer([final_input_text], return_tensors="pt")
+                final_input = {k: v.cuda() for k, v in final_input.items()}
+
+                with torch.inference_mode():
+                    outputs = model.generate(**final_input, do_sample=True, top_p=0.75, top_k=None, max_length=512)
+                outputs = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+                prefix += " " + outputs[0]
+                output_text += " " + outputs[0]
+
+            paraphrase_outputs_k[f"lex_{lex}_order_{order}"] = {
+                "final_input": final_input_text,
+                "output": [output_text.strip()],
+                "lex": lex,
+                "order": order
+            }
+        dd[args.para_dst] = paraphrase_outputs_k
+    with open(output_file, "a") as f:
+        f.write(json.dumps(dd) + "\n")
---
+++ slurm_scripts/detect-rpp.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH      --job-name="detectai"
+#SBATCH      --mail-user="weiqingh@sas.upenn.edu"
+#SBATCH      --time=12:00:00
+#SBATCH      --mem=80G
+#SBATCH      --gpus=a40
+#SBATCH      --mail-type=ALL
+#SBATCH      --array=1-4
+#SBATCH      --output=slurm_output/output_%A_%a.log
+
+
+
+
+source activate detectai
+
+cd /cbica/home/hewei/projects/detect_framework
+
+# Define data and model parameters based on the task ID
+case $SLURM_ARRAY_TASK_ID in
+  1)
+    DATA="data/gpt-4o-mini.jsonl_pp"
+    ;;
+  2)
+    DATA="data/gpt2_xl.jsonl_pp"
+    ;;
+  3)
+    DATA="data/gpt3.jsonl_pp"
+    ;;
+  4)
+    DATA="data/opt_13b.jsonl_pp"
+    ;;
+esac
+
+# Run the Python script with parameters
+python recursive_paraphrase.py --output_file $DATA
+
