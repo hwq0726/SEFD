10ddd3b4aa2eeeb3acf57c6a80308fa7f08da912
Weiqing He <weiqing726@outlook.com>
2024-09-04 20:12:55 -0400

    recursive paraphrase slurm

--- slurm_scripts/detect-rpp.sh
+++ slurm_scripts/detect-rpp.sh
@@ -18,19 +18,19 @@ cd /cbica/home/hewei/projects/detect_framework
 # Define data and model parameters based on the task ID
 case $SLURM_ARRAY_TASK_ID in
   1)
-    DATA="data/gpt-4o-mini.jsonl_pp"
+    DATA="data/gpt-4o-mini.jsonl_pp_pp2"
     ;;
   2)
-    DATA="data/gpt2_xl.jsonl_pp"
+    DATA="data/gpt2_xl.jsonl_pp_pp2"
     ;;
   3)
-    DATA="data/gpt3.jsonl_pp"
+    DATA="data/gpt3.jsonl_pp_pp2"
     ;;
   4)
-    DATA="data/opt_13b.jsonl_pp"
+    DATA="data/opt_13b.jsonl_pp_pp2"
     ;;
 esac
 
 # Run the Python script with parameters
-python recursive_paraphrase.py --output_file $DATA
+python recursive_paraphrase.py --output_file $DATA --para_src "paraphrase_outputs_2" --para_dst "paraphrase_outputs_3"
 
